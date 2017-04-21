241f3fc60d726a2638ece10f34e517fc2e980cfe
lyu <lyu@digitalriver.com>
2017-04-21 10:16:15 +0800

    Kick Off

---
+++ src/main/java/louis/coursera/UCSD_OOP_Java/App.java
@@ -0,0 +1,13 @@
+package louis.coursera.UCSD_OOP_Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
